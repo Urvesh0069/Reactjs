b073a285dda0f8ec904a0c988566c5c6c8d70950
Urvesh0069 <urveshghodadara82@gmail.com>
2025-08-19 13:15:48 +0530

    api hunter

---
+++ api_hunter/src/APIs/crud.jsx
@@ -0,0 +1,30 @@
+
+export const getUsers = async () => {
+  const response = await fetch("http://localhost:5000/users");
+  if (!response.ok) throw new Error("Failed to fetch users");
+  return response.json();
+};
+
+export const adduser = async (user) => {
+  const response = await fetch("http://localhost:5000/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(user),
+  });
+  return response.json();
+};
+
+export const deleteUser = async (id) => {
+  await fetch(`http://localhost:5000/users/${id}`, {
+    method: "DELETE",
+  });
+};
+
+export const updateUser = async (id, user) => {
+  const response = await fetch(`http://localhost:5000/users/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(user),
+  });
+  return response.json();
+};
---
+++ api_hunter/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css' // Assuming you have a CSS file for global styles
+import Form from './pages/form'
+
+const App = () => {
+  return (
+    <div>
+      <Form/>
+    </div>
+  )
+}
+
+export default App
